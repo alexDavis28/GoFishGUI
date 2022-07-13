6be640d61f8c218b19e27170b5f79979e4d10240
8m3xl <8m3xlt@gmail.com>
2022-07-13 16:24:28 +0100

    Add project files.

---
+++ GoFishGUI/PlayerHand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoFishGUI
+{
+    class PlayerHand : GoFishHand
+    {
+        public PlayerHand()
+        {
+            HandType = "Player";
+        }
+
+        public override int RequestCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
